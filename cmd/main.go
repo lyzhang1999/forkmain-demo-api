29712814148255258f8ac73c587207de5d36fe66
Zhenwei <jack230230@gmail.com>
2022-08-08 14:38:18 +0800

    fix env

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/hellokvn/go-gin-api-medium/pkg/books"
 	"github.com/hellokvn/go-gin-api-medium/pkg/common/db"
@@ -8,17 +12,31 @@ import (
 )
 
 func main() {
-	viper.SetConfigFile("./pkg/common/envs/.env")
-	viper.ReadInConfig()
 
-	port := viper.Get("PORT").(string)
-	dbUrl := viper.Get("DB_URL").(string)
+	viper.AutomaticEnv()
+
+	port := viper.GetString("Port")
+	dbUrl := fmt.Sprintf(
+		"postgres://%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		viper.GetString("DatabaseUser"),
+		viper.GetString("DatabasePassword"),
+		viper.GetString("DatabaseHost"),
+		viper.GetString("DatabasePort"),
+		viper.GetString("DatabaseName"),
+	)
 
 	r := gin.Default()
 	h := db.Init(dbUrl)
 
 	books.RegisterRoutes(r, h)
-	// register more routes here
 
-	r.Run(port)
+	r.GET("/health", func(c *gin.Context) {
+		c.String(http.StatusOK, "OK")
+	})
+
+	r.GET("/env", func(c *gin.Context) {
+		c.String(http.StatusOK, os.Getenv("ENV_NAME"))
+	})
+
+	r.Run("0.0.0.0:" + port)
 }
