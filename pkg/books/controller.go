d2f1247905270cbd5c5694fd51474c1683145258
Zhenwei <jack230230@gmail.com>
2022-08-12 16:56:57 +0800

    add init data

--- pkg/books/controller.go
+++ pkg/books/controller.go
@@ -2,6 +2,7 @@ package books
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/hellokvn/go-gin-api-medium/pkg/common/models"
 
 	"gorm.io/gorm"
 )
@@ -21,4 +22,22 @@ func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
 	routes.GET("/:id", h.GetBook)
 	routes.PUT("/:id", h.UpdateBook)
 	routes.DELETE("/:id", h.DeleteBook)
+
+	addFirstBook(db)
+}
+
+func addFirstBook(db *gorm.DB) {
+	h := &handler{
+		DB: db,
+	}
+	var book = models.Book{}
+	var count int64 = 0
+	h.DB.Model(&models.Book{}).Where("id = ?", 1).Count(&count)
+
+	if count == 0 {
+		book.Title = "War and Peace"
+		book.Author = "Leo Tolstoy"
+		book.Description = "War and Peace is a literary work mixed with chapters on history and philosophy by the Russian author Leo Tolstoy. It was first published serially, then published in its entirety in 1869."
+		h.DB.Create(&book)
+	}
 }
